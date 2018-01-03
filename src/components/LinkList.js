ffaaea074053a2e03554f2309402b27d10d7eee9
Roman Brito <romanbrito1@gmail.com>
2018-01-02 22:29:07 -0600

    react relay initial commit fetching from graphcool

---
+++ src/components/LinkList.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {
+  createFragmentContainer,
+  graphql
+} from 'react-relay'
+import Link from './Link'
+
+
+class LinkList extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.viewer.allLinks.edges.map(({node}) =>
+          <Link key={node.__id} link={node}/>
+        )}
+      </div>
+    )
+  }
+
+}
+
+export default createFragmentContainer(LinkList,
+  {
+    viewer: graphql`
+        fragment LinkList_viewer on Viewer {
+            allLinks(last: 100, orderBy: createdAt_DESC) @connection(key: "LinkList_allLinks", filters: []) {
+                edges {
+                    node {
+                        ...Link_link
+                    }
+                }
+            }
+        }
+    `
+  }
+)
